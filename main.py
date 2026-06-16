afcdd77f8f5358d09072c1627e2bc54ac3abad69
leonardo <leoinacio1997@gmail.com>
2026-06-16 18:40:20 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from locale import windows_locale
+
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(800, 600))
+print('Setup End')
+
+print('loop Start')
+while True:
+   # Check todos os eventos
+   for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        pygame.quit() #fechar a janela
+        quit() 
