431f9eaad2055b1f3685c2f4459827adc8ca9863
leonardo <leoinacio1997@gmail.com>
2026-06-29 14:30:36 -0300

    Level 2 implementation

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -11,20 +11,26 @@ from code.player import Player
 class EntityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position=(0,0)):
+    def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
                 for i in range(7):
-                    list_bg.append(Background(name=f'Level1Bg{i}', position=(0,0)))
+                    list_bg.append(Background(name=f'Level1Bg{i}', position=(0, 0)))
                     list_bg.append(Background(name=f'Level1Bg{i}', position=(WIN_WIDTH, 0)))
                 return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(name=f'Level2Bg{i}', position=(0, 0)))
+                    list_bg.append(Background(name=f'Level2Bg{i}', position=(WIN_WIDTH, 0)))
+                return list_bg
             case 'Player1':
-                return Player('Player1', (10, WIN_HEIGHT/2 - 30))
+                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
-                return Player('Player2', (10, WIN_HEIGHT/2 + 30))
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
             case 'Enemy1':
                 return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(0, WIN_HEIGHT - 40)))
-            case'Enemy2':
+            case 'Enemy2':
                 return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(0, WIN_HEIGHT - 40)))
         return None
