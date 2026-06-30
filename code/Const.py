00b07ff9b457d817761bac672151c52e4f8f817d
leonardo <leoinacio1997@gmail.com>
2026-06-29 22:07:46 -0300

    SQLite3 implementation for Score using proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -6,6 +6,7 @@ C_BLACK = (0, 0, 0)
 C_WHITE = (255, 255, 255)
 C_GREEN = (0, 128, 0)
 C_CYAN = (0, 128, 128)
+C_YELLOW = (255, 255, 0)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -100,7 +101,6 @@ ENTITY_SCORE = {
     'Enemy2': 125,
     'Enemy2Shot': 0,
 
-
 }
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
@@ -132,9 +132,28 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 SPAWN_TIME = 4000
 
 # T
-TIMEOUT_STEP = 100 #100ms
-TIMEOUT_LEVEL = 20000 #20s
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
+
+
